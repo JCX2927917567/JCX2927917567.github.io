40c212e5390bcf25cc3fffc480581faa1e7ac74e
JC-Xiong <2927917567@qq.com>
2023-08-21 22:00:54 +0800

    Site updated: 2023-08-21 22:00:49

--- custom/js/home_top.js
+++ custom/js/home_top.js
@@ -1,14 +1,18 @@
-// $(".topGroup").hover((function() {}
-// ), (function() {
-//         document.getElementById("todayCard").classList.remove("hide"),
-//         document.getElementById("todayCard").style.zIndex = 1
-//     }
-// ))
-
+// 鼠标移出隐藏推荐卡片
 var topGroup = document.querySelector(".topGroup");
 var todayCard = document.getElementById("todayCard");
-
-topGroup.addEventListener("mouseleave", function() {
+topGroup.addEventListener("mouseleave", function () {
   todayCard.classList.remove("hide");
+  topGroup.classList.remove("toScroll");
   todayCard.style.zIndex = 1;
 });
+
+
+let toHide = () => {
+  (window.event.cancelBubble = !0),
+    document.getElementById("todayCard") &&
+    document.getElementById("todayCard").classList.add("hide");
+    document.getElementById("topGroup") &&
+    document.getElementById("topGroup").classList.add("toScroll");
+    console.log(document.getElementById("topGroup").classList);
+};
